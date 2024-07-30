4e1a8179034811b69429e4e9ad8476c9813c858e
bym0701 <100143364+bym0701@users.noreply.github.com>
2024-07-30 21:52:33 +0900

    Add files via upload

--- test.java
+++ test.java
@@ -3,6 +3,7 @@ package test;
 public class test {
 	public static void main(String[] args) {
 		//[1-1]
+		/*
 		int x=15;
 		if(x > 10 && x < 20) System.out.println("[1-1]1.");
 		
@@ -24,7 +25,7 @@ public class test {
 		
 		String str1 = "yes";
 		if(str1.equals("yes")) System.out.println("[1-1]8.");
-		
+		*/
 		//[1-2]
 		int sum1 = 0;
 		for(int i = 1;  i <= 20; i++) {
@@ -34,49 +35,69 @@ public class test {
 		}
 		System.out.println("[1-2]" + sum1);
 		
+		//[1-3]
+		
+		int sum2 = 0;
+		int sum = 0;
+		
+		for(int i = 1; i <= 10; i++) {
+			sum += i;
+			sum2 += sum;
+		}
+		System.out.println("[1-3]" + sum2);
+		
 		//[1-4]
-		//int sum2 = 0;
-		//int count1 = 0;
-		//for(int j = 1; sum2 <= 100; j++) {
-		//	if(j % 2 == 1) j *= -1;
-		//	sum2 += j;
-		//	count1++;
-		//}
-		//System.out.println("[1-4]" + count1);
+		int sum3 = 0;
+		int count1 = 1;
+		int delta = 1;
+
+		while(true) {
+			sum3 += count1*delta;
+			if(sum3 >= 100) break;
+			count1++;
+			delta = -delta;
+		}
+		
+		System.out.println("[1-4]" + count1);
 		
 		//[1-5]
 		System.out.println("[1-5]");
-		for(int i = 1; i <= 6; i++) {
+		/* for(int i = 1; i <= 6; i++) {
 			for(int j = 1; j <= 6; j++) {
 				if(i+j == 6) System.out.println(i + "+" + j + "= 6");
 			}
+		} //
+		*/
+		
+		for(int i = 1; i <= 6; i++) {
+			int j = 6-i;
+			if(0<j && j<6) System.out.println(i + "+" + j + "= 6");
 		}
 		
-		//[1-6]
-		//for(int y = 0; y<10; y++) {
-		//	for(int x1 = 0; x1<=y; x1++)
-		//		System.out.print("*");
-		//	System.out.println();
-		//}
 		
-		//int y = 0;
-		//int x1 = 0;
-		//while(y<10) {
-		//	while(x1 <= y) {
-		//		System.out.print("*");
-		//		x1++;
-		//	}
-		//	y++;
-		//	System.out.println();
-		//}
+		//[1-6]
+		System.out.println("[1-6]");
+		
+		int y = 0;
+		while(y<10) {
+			int x1 = 0;
+			while(x1 <= y) {
+				System.out.print("*");
+				x1++;
+			}
+			System.out.println();
+			y++;
+		}
 		
 		//[1-7]
 		String str = "41389";
-		int sum2 = 0;
+		int sum4 = 0;
 		for(int i = 0; i<str.length(); i++) {
 			
+			sum4 += str.charAt(i) - '0';
+			
 		}
-		System.out.println("sum = " + sum2);
+		System.out.println("[1-7]sum = " + sum4);
 		
 	}
 }
--- 박유민/LogicOperation.java
+++ 박유민/LogicOperation.java
@@ -6,5 +6,27 @@ public class LogicOperation {
 		int x = 3, y = 9;
 		
 		System.out.println("논리 AND 테스트");
+		System.out.println( (x < y) + "," + (x > 0) + ":" + (x < y && x > 0));
+		System.out.println( (x > y) + "," + (x > 0) + ":" + (x > y && x > 0));
+		System.out.println( (x < y) + "," + (x < 0) + ":" + (x < y && x < 0));
+		System.out.println( (x > y) + "," + (x < 0) + ":" + (x > y && x < 0));
+		
+		System.out.println("논리 OR 테스트");
+		System.out.println( (x < y) + "," + (x > 0) + ":" + (x < y || x > 0));
+		System.out.println( (x > y) + "," + (x > 0) + ":" + (x > y || x > 0));
+		System.out.println( (x < y) + "," + (x < 0) + ":" + (x < y || x < 0));
+		System.out.println( (x > y) + "," + (x < 0) + ":" + (x > y || x < 0));
+		
+		System.out.println("논리 XOR 테스트");
+		System.out.println( (x < y) + "," + (x > 0) + ":" + (x < y ^ x > 0));
+		System.out.println( (x > y) + "," + (x > 0) + ":" + (x > y ^ x > 0));
+		System.out.println( (x < y) + "," + (x < 0) + ":" + (x < y ^ x < 0));
+		System.out.println( (x > y) + "," + (x < 0) + ":" + (x > y ^ x < 0));
+		
+		System.out.println("논리 NOT 테스트");
+		System.out.println( (x < y) + ":" + !(x < y));
+		System.out.println( (x > y) + ":" + !(x > y));
+
+		
 	}
 }
