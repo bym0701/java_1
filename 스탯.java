8d4a55c42dd3f8fdf431aad086845cbac35719e2
bym0701 <100143364+bym0701@users.noreply.github.com>
2024-10-28 19:53:22 +0900

    Add files via upload

--- 스탯.java
+++ 스탯.java
@@ -7,25 +7,31 @@ public class 스탯 {
 	private int 체력;
 	private int 공격력;
 	private int 방어력;
+	private String 색깔;
+	private String[] color = {"파란색", "노란색", "빨간색"};
 	
-	public void Random() {
-		Random rnd = new Random();
+	Random rnd = new Random();
+	public void Random_스탯() {
 		this.체력 = rnd.nextInt(1000);
 		this.레벨 = rnd.nextInt(10);
 		this.공격력 = rnd.nextInt(500);
 		this.방어력 = rnd.nextInt(500);
+		this.색깔 = color[rnd.nextInt(3)];
+		System.out.println("랜덤 스탯 성공");
 	}
-	public int get레벨() {
-		return this.레벨;
+	public void Show_스탯() {
+		
 	}
-	public int get체력() {
-		return this.체력;
-	}
-	public int get공격력() {
-		return this.공격력;
-	}
-	public int get방어력() {
-		return this.방어력;
-	}
+	public int get레벨() {return this.레벨;}
+	public int get체력() {return this.체력;}
+	public int get공격력() {return this.공격력;}
+	public int get방어력() {return this.방어력;}
+	public String get색깔() {return this.색깔;}
+	
+	public void set레벨(int n) {this.레벨 = n;}
+	public void set체력(int n) {this.체력 = n;}
+	public void set공격력(int n) {this.공격력 = n;}
+	public void set방어력(int n) {this.방어력 = n;}	
+	public void set색깔(String n) {this.색깔 = n;}
 	
 }
--- 용사키우기.java
+++ 용사키우기.java
@@ -1,8 +1,8 @@
 package 용사키우기;
-
+import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
-
 public class 용사키우기 extends 스탯{
 	public static void main(String[] args) {
 		용사키우기 용사키우기 = new 용사키우기();
@@ -13,18 +13,24 @@ public class 용사키우기 extends 스탯{
 		int input;
 		
 		while(true) {
-			System.out.println("1을 입력하면 스탯이 랜덤으로 바뀝니다. (종료 : -1) : ");
+			Show_스탯();
+			System.out.println("1. 랜덤 스탯");
+			System.out.println("2. Save");
+			System.out.println("3. Load");
+			System.out.println("번호를 입력해주세요 (종료 : -1) : ");
 			input = sc.nextInt();
-			if(input == 1) {
-				Random();
+			if(input == 1)
+				Random_스탯();
+				
+			else if(input == 2)
 				Save();
-			}
-				
-			else if(input == -1) {
-				System.out.println("종료");
+			
+			else if(input == 3)
+				Load();
+
+			else if(input == -1)
 				break;
-			}
-				
+			
 			else
 				System.out.println("다시 입력해주세요.");
 		}
@@ -32,19 +38,27 @@ public class 용사키우기 extends 스탯{
 	public void Save() {
 		try {
 			PrintWriter out = new PrintWriter("용사.txt");
-			out.println("1. 레벨 : " + get레벨());
-			out.println("2. 체력 : " + get체력());
-			out.println("3. 공격력 : " + get공격력());
-			out.println("4. 방어력 : " + get방어력());
+			out.println(get레벨()+"::"+get체력()+"::"+get공격력()+"::"+get방어력()+"::"+get색깔());
 			System.out.println("저장 성공");
 			out.close();
-
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		System.out.println("1. 레벨 : " + get레벨());
-		System.out.println("2. 체력 : " + get체력());
-		System.out.println("3. 공격력 : " + get공격력());
-		System.out.println("4. 방어력 : " + get방어력());
+	}
+	public void Load() {
+		try {
+			File loadFile = new File("용사.txt");
+			Scanner sc= new Scanner(loadFile);
+			sc.useDelimiter("::");
+			스탯 s = new 스탯();
+			s.set레벨(sc.nextInt());
+			s.set체력(sc.nextInt());
+			s.set공격력(sc.nextInt());
+			s.set방어력(sc.nextInt());
+			s.set색깔(sc.next());
+			sc.close();
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
